18f556f6483db19fcfd73236de30fb0078c87181
Carlos Lorenzo <carloren.1996@gmail.com>
2025-07-02 08:03:56 +0000

    30-06

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
 
 	return (
 		<div className="container text-end mt-5 w-50">
-			<Link to="/add-contact"><button className="btn btn-primary">Añadir Contacto</button></Link>
+			<Link to="/add-contact" className="btn btn-primary">Añadir Contacto</Link>  {/* Basta con poner una clase de botón en la etiqueta link, no hace falta añadir etiqueta botón */}
 			<Cards />
 		</div>
 	);
